a85596efe68ac65c0e4091b0e6f1b4229555e428
Khubaib Ahmad <khubaib.ahmad@visable.com>
2025-05-23 14:51:25 +0200

    local runner

---
+++ src/http_client/local_runner.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include "http_client.hpp"
+
+int main()
+{
+
+    boost::asio::io_context io_context;
+
+    auto dummyWork = std::make_unique<boost::asio::executor_work_guard<boost::asio::io_context::executor_type>>(
+        boost::asio::make_work_guard(io_context)
+    );
+
+    http_client::header::client_header header(io_context);
+    header();
+
+
+    std::thread t([&io_context](){
+        try{
+            io_context.run();
+        }
+        catch (std::exception& e) {
+            std::cerr << "Exception: " << e.what() << "\n";
+        } });
+    std::cout << "Hello, World! from Client" << std::endl;
+    return 0;
+}
