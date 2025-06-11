60ea2c838a8efed6d3c873911e026a1c248f3238
Khubaib Ahmad <khubaib.ahmad@visable.com>
2025-06-11 03:09:40 +0200

    added promise and merged connect and send

--- src/http_client/core/client.hpp
+++ src/http_client/core/client.hpp
@@ -7,7 +7,7 @@
 #include "connection.hpp"
 
 namespace http_client::client {
-    struct _http_client {
+    struct _http_client : std::enable_shared_from_this<http_client::client::_http_client> {
         static std::shared_ptr<_http_client> make(http_client::parameter::client_parameter parameter) {
             return std::shared_ptr<_http_client>(new _http_client(std::move(parameter)));
         }
@@ -16,36 +16,63 @@ namespace http_client::client {
         void send_async(const request::http_request &request, T func) {
             boost::asio::post(_parameter._executor, [callback = std::move(func)]()mutable {
                 http_client::response::http_response response{};
-                response.body = "Hello from client.hpp line 20";
+                response.body = "Hello from client.hpp line 19";
                 callback(std::error_code(), std::move(response));
             });
         }
 
         template<typename T>
-        void connect_async(const http_client::connection::client_connection connection, T func) {
+        void connect_async(
+            const http_client::connection::client_connection connection,
+            const request::http_request &request,
+            T func
+        ) {
             _resolver.async_resolve(
                 connection.host,
                 connection.port,
-                [this, callback = std::move(func)](
-            const boost::system::error_code &ec,
+                [
+                    localThis = this->shared_from_this(),
+                    callback = std::move(func),
+                    request
+                ](
+            const boost::system::error_code ec,
             const boost::asio::ip::tcp::resolver::results_type &results
-        )mutable {
+        ) mutable {
                     if (ec) {
-                        std::cout << "Error: " << ec.message() << "\n";
-                        return callback(ec);
+                        std::cout << "Error resolving: " << ec.message() << "\n";
+                        return callback(ec, http_client::response::http_response{});
                     }
-                    std::cout << "+++++++++++ Resolved successfully.\n";
-                    return callback(std::error_code());
+                    boost::asio::async_connect(
+                        this->_socket,
+                        results,
+                        [
+                            callback_ac = std::move(callback),
+                            localThis = std::move(localThis),
+                            request
+                        ](
+                    const boost::system::error_code ec,
+                    const boost::asio::ip::tcp::endpoint endpoint
+                ) {
+                            if (ec) {
+                                std::cout << "Error : " << ec.message() << "\n";
+                                return callback_ac(ec, http_client::response::http_response{});
+                            }
+                            std::cout << "Connected successfully to " << endpoint << "\n";
+
+                            localThis->send_async(request, std::move(callback_ac));
+                        });
                 });
         }
 
     private:
         http_client::parameter::client_parameter _parameter;
         boost::asio::ip::tcp::resolver _resolver;
+        boost::asio::ip::tcp::socket _socket;
 
         explicit _http_client(http_client::parameter::client_parameter parameter)
             : _parameter(std::move(parameter)),
-              _resolver(_parameter._executor) {
+              _resolver(_parameter._executor),
+              _socket(_parameter._executor) {
         }
     };
 };
--- src/http_client/core/runner.hpp
+++ src/http_client/core/runner.hpp
@@ -16,53 +16,36 @@ namespace http_client::runner {
         void operator()() {
             http_client::parameter::client_parameter params;
             params._executor = _executor;
-            auto client = http_client::client::_http_client::make(std::move(params));
+            const std::shared_ptr<http_client::client::_http_client> client = http_client::client::_http_client::make(
+                std::move(params));
 
 
             http_client::connection::client_connection connection;
             connection.host = "google.com";
             connection.port = "80";
 
-            std::promise<std::error_code> connect_promise;
-            auto connect_future = connect_promise.get_future();
+            http_client::request::http_request request;
+            std::promise<std::pair<std::error_code, http_client::response::http_response> > operation_promise;
+            auto operation_future = operation_promise.get_future();
 
-            client->connect_async(std::move(connection), [&connect_promise](std::error_code err) {
+            client->connect_async(std::move(connection), request, [
+                                      &operation_promise
+                                  ](
+                              std::error_code err,
+                              http_client::response::http_response response
+                          ) {
+                                      operation_promise.set_value(std::make_pair(err, std::move(response)));
+                                  });
 
-                if (err) {
-                    std::cerr << "Connection error: " << err.message() << std::endl;
-                } else {
-                    std::cout << "Connected successfully!" << std::endl;
-                }
-                connect_promise.set_value(err);
-            });
+            auto [error, response] = operation_future.get();
 
-            if (std::error_code connect_result = connect_future.get()) {
-                std::cerr << "Failed to connect: " << connect_result.message() << std::endl;
+            if (error) {
+                std::cerr << "Operation failed: " << error.message() << std::endl;
                 return;
             }
 
-            http_client::request::http_request request;
-            std::promise<std::error_code> send_promise;
-            auto send_future = send_promise.get_future();
-            client
-                    ->
-                    send_async(request, [&send_promise](std::error_code err,
-                                                        http_client::response::http_response response) {
-                                   if (err) {
-                                       std::cerr << "Error: " << err.message() << std::endl;
-                                       send_promise.set_value(err);
-                                   } else {
-                                       std::cout << "Response body: " << response.body << std::endl;
-                                       std::cout << "Response status code: " << response.status_code << std::endl;
-                                       send_promise.set_value(std::error_code());
-                                   }
-                               }
-
-                    );
-            std::error_code send_result = send_future.get();
-            if (send_result) {
-                std::cerr << "Failed to send request: " << send_result.message() << std::endl;
-            }
+            std::cout << "Response body: " << response.body << std::endl;
+            std::cout << "Response status code: " << response.status_code << std::endl;
         }
     };
 }
